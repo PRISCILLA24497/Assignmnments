a6646d11bfc3b66cc20afd09e3f31cd0b27cc71c
PRISCILLA24497 <59496395+PRISCILLA24497@users.noreply.github.com>
2020-10-01 22:46:00 +0530

    Add files via upload

---
+++ sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumofdigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  sum=0, r;
+            Console.WriteLine("Enter the value of n:");
+           int  n =Convert.ToInt32(Console.ReadLine());
+            while (n > 0)
+            {
+                r = n % 10;
+                sum = sum + r;
+                n = n / 10;
+            }
+            Console.WriteLine($"The sum of the digits of given number is: {sum}");
+            Console.ReadLine();
+
+        }
+    }
+}
