939f3e6ac44a8cf96d1e31adfe24e3bc1f0aa402
PRISCILLA24497 <59496395+PRISCILLA24497@users.noreply.github.com>
2020-10-03 23:19:42 +0530

    Add files via upload

---
+++ FileHandleExample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Directoryex
+{
+      class FW
+        {
+
+
+            public void Myfile()
+            {
+                FileStream f = new FileStream("C:\\Users\\pthota21\\Desktop\\sweety\\java.txt", FileMode.OpenOrCreate);
+
+                StreamWriter sw = new StreamWriter(f);
+
+                Console.WriteLine("Enter the Text:");
+
+
+                // To read the input from the user 
+                string s = Console.ReadLine();
+                sw.WriteLine(s);
+                sw.Close();
+                f.Close();
+            }
+           
+
+
+                class Program
+                {
+
+
+                    static void Main(string[] args)
+                    {
+
+                        //  var currentDirectory = Directory.GetCurrentDirectory();
+                        //Console.WriteLine(currentDirectory);
+                        //Console.ReadLine();
+                        //Console.WriteLine(Directory.GetDirectoryRoot(currentDirectory));
+                        //Console.ReadLine();
+
+                        var docpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                        var Dirname = $@"{docpath}\sweety";
+                        DirectoryInfo dinfo = Directory.CreateDirectory(Dirname);
+                        Console.WriteLine($"Complete name of the directory:{dinfo.FullName},Name:{dinfo.Name},Parent:{dinfo.Parent}");
+                        Console.ReadLine();
+
+
+
+                        FW w = new FW();
+
+                        w.Myfile();
+
+
+                        Console.ReadLine();
+
+                    }
+                }
+            }
+        }
+  
