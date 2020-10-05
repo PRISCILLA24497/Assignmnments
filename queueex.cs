77de630ddba0e014dcadd62199b046862a384c04
PRISCILLA24497 <59496395+PRISCILLA24497@users.noreply.github.com>
2020-10-05 23:41:38 +0530

    Add files via upload

---
+++ queueex.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queuesample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> qu = new Queue<int>();
+            qu.Enqueue(2);
+            qu.Enqueue(4);
+            qu.Enqueue(6);
+            qu.Enqueue(8);
+            qu.Enqueue(10);
+            qu.Enqueue(12);
+            qu.Enqueue(14);
+            qu.Enqueue(16);
+            qu.Enqueue(18);
+            qu.Enqueue(20);
+
+            Console.WriteLine("The elements are in the Queue after Enqueue are:");
+            foreach (int q in qu)
+            {
+                Console.WriteLine(q);
+
+            }
+
+            Console.WriteLine("Count is" + qu.Count());
+            Console.WriteLine("The peek element of Queue is:" + qu.Peek());
+            Console.ReadLine();
+            qu.Dequeue();
+            qu.Dequeue();
+            qu.Dequeue();
+            qu.Dequeue();
+            qu.Dequeue();
+            qu.Dequeue();
+            //elements in the Queue after performing dequeue for 6 times
+            Console.WriteLine("checking value in Queue"+qu.Contains(4));
+            Console.WriteLine("Count is" + qu.Count());
+            Console.WriteLine("elements of Queue after Dequeue:");
+            foreach (int x in qu)
+            {
+
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("The peek element of Queue is:" + qu.Peek());
+            Console.ReadLine();
+            Console.WriteLine("The left over elements in the Queue are:");
+            foreach (int x in qu)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+            qu.Enqueue(22);
+            qu.Enqueue(24);
+            qu.Enqueue(26);
+            Console.WriteLine("Count is" + qu.Count());
+            //elements in the stack after performing pop for 5 times
+
+
+            Console.WriteLine("The left over elements in the Queue are:");
+            foreach (int x in qu)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+            qu.Dequeue();
+            qu.Dequeue();
+            qu.Dequeue();
+            qu.Dequeue();
+            qu.Dequeue();
+            //elements in the Queue after performing dequeue for 5 times
+            Console.WriteLine("checking value in Queue:" + qu.Contains(10));
+            Console.WriteLine("The left over elements in the Queue are:");
+            foreach (int s in qu)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+
+
+            Console.WriteLine("elements of Queue after Dequeue:");
+            foreach (int x in qu)
+            {
+
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("The peek element of Queue is:" + qu.Peek());
+            Console.ReadLine();
+            Console.WriteLine("Count is" + qu.Count());
+
+
+
+             Console.WriteLine("The left over elements in the Queue are:");
+            foreach (int s in qu)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+
+           
+            Console.WriteLine("Count is" + qu.Count());
+            Console.WriteLine("elements of Queue after Dequeue:");
+
+            qu.Enqueue(28);
+            qu.Enqueue(30);
+            qu.Enqueue(32);
+            Console.WriteLine("The left over elements in the Queue are:");
+            foreach (int s in qu)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+
+            //elements in the Queue after performing dequeue for 4 times
+            Console.WriteLine("checking value in Queue:" + qu.Contains(32));
+            Console.WriteLine("Count is" + qu.Count());
+            Console.WriteLine("elements of Queue after Dequeue:");
+
+            qu.Dequeue();
+            qu.Dequeue();
+            qu.Dequeue();
+            qu.Dequeue();
+
+            Console.WriteLine("The left over elements in the Queue are:");
+            foreach (int x in qu)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("Count is" + qu.Count());
+            foreach (int x in qu)
+            {
+
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("The peek element of Queue is:" + qu.Peek());
+            Console.ReadLine();
+            qu.Enqueue(34);
+            qu.Enqueue(36);
+            qu.Enqueue(38);
+            qu.Enqueue(40);
+
+
+            Console.WriteLine("The left over elements in the queue are:");
+            foreach (int x in qu)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("Count is" + qu.Count());
+            //elements in the Queue after performing dequeue for 3 times
+
+            qu.Dequeue();
+            qu.Dequeue();
+            qu.Dequeue();
+            Console.WriteLine("checking value in Queue:" + qu.Contains(40));
+            Console.WriteLine("The left over elements in the Queue are:");
+            foreach (int x in qu)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("Count is" + qu.Count());
+            Console.WriteLine("elements of queue after dequeue:");
+
+            foreach (int x in qu)
+            {
+
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("The peek element of queue is:" + qu.Peek());
+            Console.ReadLine();
+
+            qu.Enqueue(42);
+            qu.Enqueue(44);
+            qu.Enqueue(46);
+
+
+
+            Console.WriteLine("The left over elements in the Queue are:");
+            foreach (int x in qu)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("elements of Queue after dequeue:");
+            //elements in the Queue after performing dequeue for 2 times
+
+            qu.Dequeue();
+            qu.Dequeue();
+            Console.WriteLine("The left over elements in the Queue are:");
+
+            Console.WriteLine("Count is" + qu.Count());
+
+
+
+            foreach (int x in qu)
+            {
+
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("checking value in Queue:" + qu.Contains(44));
+            Console.WriteLine("Count is" + qu.Count());
+            Console.WriteLine("The peek element of Queue is:" + qu.Peek());
+            Console.ReadLine();
+            Console.WriteLine("The left over elements in the Queue are:");
+            foreach (int s in qu)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+      
+
+
+
+
---
+++ stex.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackSample
+{
+    class stex
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> st = new Stack<int>();
+            st.Push(2);
+            st.Push(4);
+            st.Push(6);
+            st.Push(8);
+            st.Push(10);
+            st.Push(12);
+            st.Push(14);
+            st.Push(16);
+            st.Push(18);
+            st.Push(20);
+
+            Console.WriteLine("The elements are in the stack after push are:");
+            foreach (int s in st)
+            {
+                Console.WriteLine(s);
+
+            }
+            
+              Console.WriteLine("Count is" + st.Count());
+            Console.WriteLine("The peek element of stack is:" + st.Peek());
+            Console.ReadLine();
+            //elements in the stack after performing pop for 6 times
+
+           
+            st.Pop();
+            st.Pop();
+            st.Pop();
+            st.Pop();
+            st.Pop();
+            st.Pop();
+
+            Console.WriteLine("Count is" + st.Count());
+            Console.WriteLine("elements of stack after pop:");
+            foreach (int x in st)
+            {
+
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("The peek element of stack is:" + st.Peek());
+            Console.ReadLine();
+            Console.WriteLine("The left over elements in the stack are:");
+            foreach (int s in st)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        
+        st.Push(22);
+            st.Push(24);
+            st.Push(26);
+            Console.WriteLine("Count is" + st.Count());
+            //elements in the stack after performing pop for 5 times
+            Console.WriteLine("The left over elements in the stack are:");
+            foreach (int s in st)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        
+
+        st.Pop();
+            st.Pop();
+            st.Pop();
+            st.Pop();
+            st.Pop();
+            Console.WriteLine("The left over elements in the stack are:");
+            foreach (int s in st)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        
+
+        Console.WriteLine("elements of stack after pop:");
+            foreach (int x in st)
+            {
+
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("The peek element of stack is:" + st.Peek());
+            Console.ReadLine();
+            Console.WriteLine("Count is" + st.Count());
+
+            st.Push(28);
+            st.Push(30);
+            st.Push(32);
+            Console.WriteLine("The left over elements in the stack are:");
+            foreach (int s in st)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        
+        //elements in the stack after performing pop for 4 times
+        Console.WriteLine("Count is" + st.Count());
+            Console.WriteLine("elements of stack after pop:");
+            st.Pop();
+            st.Pop();
+            st.Pop();
+            st.Pop();
+            Console.WriteLine("The left over elements in the stack are:");
+            foreach (int s in st)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        
+        Console.WriteLine("Count is" + st.Count());
+            foreach (int x in st)
+            {
+
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("The peek element of stack is:" + st.Peek());
+            Console.ReadLine();
+            st.Push(34);
+            st.Push(36);
+            st.Push(38);
+            st.Push(40);
+            Console.WriteLine("The left over elements in the stack are:");
+            foreach (int s in st)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        
+        Console.WriteLine("Count is" + st.Count());
+            //elements in the stack after performing pop for 3 times
+
+           
+            st.Pop();
+            st.Pop();
+            st.Pop();
+            Console.WriteLine("The left over elements in the stack are:");
+            foreach (int s in st)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        
+        Console.WriteLine("Count is" + st.Count());
+            Console.WriteLine("elements of stack after pop:");
+
+            foreach (int x in st)
+            {
+
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("The peek element of stack is:" + st.Peek());
+            Console.ReadLine();
+            st.Push(42);
+            st.Push(44);
+            st.Push(46);
+            Console.WriteLine("The left over elements in the stack are:");
+            foreach (int s in st)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        
+        Console.WriteLine("elements of stack after pop:");
+            //elements in the stack after performing pop for 2 times
+
+           
+            st.Pop();
+            st.Pop();
+            Console.WriteLine("The left over elements in the stack are:");
+           
+        Console.WriteLine("Count is"+st.Count());
+
+
+
+            foreach (int x in st)
+            {
+
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("Count is"+st.Count());
+            Console.WriteLine("The peek element of stack is:" + st.Peek());
+            Console.ReadLine();
+            Console.WriteLine("The left over elements in the stack are:");
+            foreach(int s in st)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
