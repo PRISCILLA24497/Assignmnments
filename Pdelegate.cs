68ac6d19578eb49c96a9ad260b8f9d78d8acb247
PRISCILLA24497 <59496395+PRISCILLA24497@users.noreply.github.com>
2020-10-08 09:36:31 +0530

    Add files via upload

---
+++ Pdelegate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Action<int, int> adddel = add;
+            //adddel(1, 2);
+           // Func<int, int, int> sumfunc = sum;
+           Func<int> sumfunc = print;
+
+
+            int s = sumfunc();
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+        public static void add(int x,int y)
+        {
+            Console.WriteLine($"{(x + y)}");
+        }
+        public static int sum(int a,int b)
+        {
+            return a + b;
+        }
+        public static int print()
+        {
+            return 100;
+        }
+
+    }
+    }
