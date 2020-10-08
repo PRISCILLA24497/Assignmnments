4b8b653b2fc41f14aa013624dbd22ee188bb5fa9
PRISCILLA24497 <59496395+PRISCILLA24497@users.noreply.github.com>
2020-10-08 15:45:52 +0530

    Add files via upload

---
+++ Form3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace print2ex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button b = new Button();
+            b.Text = "Click me";
+            b.Size = new Size(100, 50);
+            b.Location = new Point(339, 169);
+            this.Controls.Add(b);
+            b.Click += new EventHandler(button1_Click1);
+
+        }
+
+        ////using anonymous method
+        //b.Click += delegate (object sender1, EventArgs e1)
+
+        //{
+
+        //    MessageBox.Show("This is printing again!");
+        //};
+
+
+
+
+
+        private void button1_Click1(object sender1, EventArgs e1)
+        {
+
+
+            MessageBox.Show("This is printing again!");
+        }
+    }
+}
+        
