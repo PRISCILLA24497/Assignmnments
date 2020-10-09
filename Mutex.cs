a9312d216e84853d180045a1bcf1487be35c8631
PRISCILLA24497 <59496395+PRISCILLA24497@users.noreply.github.com>
2020-10-10 00:24:08 +0530

    Add files via upload

---
+++ Mutex.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Multithread_Mutex
+{
+    class Mutex
+    {
+        private static System.Threading.Mutex mutex = new System.Threading.Mutex();
+
+        static void Main(string[] args)
+        {
+           
+            //Create multiple threads to understand Mutex
+            for (int i = 1; i <= 5; i++)
+            {
+                Thread T = new Thread(MutexExample);
+                T.Name = "Thread " + i;
+                T.Start();
+            }
+            Console.ReadKey();
+        }
+        //Method to implement syncronization using Mutex  
+        static void MutexExample()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " Wants to Enter Critical Section for processing");
+            try
+            {
+                //Blocks the current thread until the current WaitOne method receives a signal.  
+                //Wait until it is safe to enter. 
+                mutex.WaitOne();
+                Console.WriteLine("Success: " + Thread.CurrentThread.Name + " is Processing now");
+                Thread.Sleep(5000);
+                Console.WriteLine("Exit: " + Thread.CurrentThread.Name + " is Completed its task");
+            }
+            finally
+            {
+                //Call the ReleaseMutex method to unblock so that other threads
+                //that are trying to gain ownership of the mutex.  
+                mutex.ReleaseMutex();
+            }
+        }
+    }
+}
+      
---
+++ Semaphore.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace MultiThread_Semaphore
+{
+    class Program
+    {
+        public static Semaphore semaphore = new Semaphore(1, 2);
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread T = new Thread(Semaphore)
+                {
+                    Name = "Thread " + i
+                };
+             T.Start(i);
+            }
+            Console.ReadKey();
+        }
+        static void Semaphore(object id)
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " Wants to Enter into Critical Section for processing");
+            try
+            {
+                //Blocks the current thread until the current WaitHandle receives a signal.   
+                semaphore.WaitOne();
+                Console.WriteLine("Success: " + Thread.CurrentThread.Name + " is Doing its work");
+                Thread.Sleep(5000);
+                Console.WriteLine(Thread.CurrentThread.Name + "Exit.");
+            }
+            finally
+            {
+                //Release() method to release semaphore  
+                semaphore.Release();
+            }
+        }
+    }
+}
+        
