df47918c357edfbb4c45c04267c76155498f281a
PRISCILLA24497 <59496395+PRISCILLA24497@users.noreply.github.com>
2020-10-10 23:57:56 +0530

    Add files via upload

---
+++ MultiThreadPerfromance.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+//*********************************************Multithread performance check***************************************************
+namespace Thread_performance
+{
+    class Thread_Perform
+    {
+        public static void Thread1()
+        {
+            long count = 0;
+            for (int i = 0; i <= 10000; i++)
+            {
+                count++;
+
+            }
+            Console.WriteLine("Number of counts" + count);
+             Console.ReadLine();
+
+        }
+
+        public static void Thread2()
+        {
+            long count = 0;
+
+            for (int i = 1; i <= 10000; i++)
+            {
+
+                count++;
+
+            }
+            Console.WriteLine("Number of counts" + count);
+
+            // Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Thread_Perform obj = new Thread_Perform();
+            Stopwatch s = new Stopwatch();
+          
+            Thread T1 = new Thread(Thread1);
+
+            Thread T2 = new Thread(Thread2);
+
+            T1.Start();
+            T2.Start();
+            T1.Join();
+            T2.Join();
+            s.Start();
+            //obj.Thread1();
+            //obj.Thread2();
+            Console.WriteLine("Stopwatch is started");
+             Console.ReadLine();
+
+            s.Stop();
+
+
+            Console.WriteLine(s.ElapsedMilliseconds);
+            Console.WriteLine("Stopwatch stopped");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singlethreadperformance.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace SingleThread_Performance
+{
+    class Thread_Perform
+    {
+        public  void Thread1()
+        {
+            long count = 0;
+            for (int i = 0; i <= 100000; i++)
+            {
+                count++;
+
+            }
+            Console.WriteLine("Number of counts" + count);
+            Console.ReadLine();
+
+        }
+
+        public  void Thread2()
+        {
+            long count = 0;
+
+            for (int i = 1; i<= 100000000; i++)
+
+
+            {
+
+                count++;
+
+            }
+            Console.WriteLine("Number of counts" + count);
+
+             Console.ReadLine();
+        }
+        
+            static void Main(string[] args)
+            {
+                Thread_Perform obj = new Thread_Perform();
+                obj.Thread1();
+                obj.Thread2();
+           
+                Stopwatch s = new Stopwatch();
+
+             
+                s.Start();
+
+                Console.WriteLine("Stopwatch is started");
+                Console.ReadLine();
+
+                s.Stop();
+
+
+                Console.WriteLine(s.ElapsedMilliseconds);
+                Console.WriteLine("Stopwatch stopped");
+            Console.ReadLine();
+            }
+        }
+    }
