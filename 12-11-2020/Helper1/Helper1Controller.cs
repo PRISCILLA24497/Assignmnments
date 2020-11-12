cb4742367c96c2c46fd9e7cfd96ffa8c702500f5
PRISCILLA24497 <59496395+PRISCILLA24497@users.noreply.github.com>
2020-11-12 23:46:55 +0530

    Add files via upload

---
+++ 12-11-2020/Helper1/Helper1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HeplersViews_Bag_tempView_Data.Controllers
+{
+    public class Helper1Controller : Controller
+    {
+        // GET: Helper1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
