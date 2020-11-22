1dda75d3e0255e061808fbad77b0ddec16f24c35
PRISCILLA24497 <59496395+PRISCILLA24497@users.noreply.github.com>
2020-11-23 00:24:33 +0530

    Add files via upload

---
+++ WebAPiCrud/MVC/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using WebApimvc.Models;
+
+namespace WebApimvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<MvcEmployeeModel> empList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employees").Result;
+            empList = response.Content.ReadAsAsync<IEnumerable<MvcEmployeeModel>>().Result;
+            return View(empList);
+        }
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+
+                return View(new MvcEmployeeModel());
+
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employees/" + id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<MvcEmployeeModel>().Result);
+             
+            }
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(MvcEmployeeModel emp)
+        {
+            if (emp.EID==0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employees", emp).Result;
+
+                TempData["Success Message"] = "Saved Successfully";
+            }
+            else {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Employees/" + emp.EID, emp).Result;
+
+
+                TempData["Success Message"] = "Updated Successfully";
+            }
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Employees/" + id.ToString()).Result;//this web api cal will insert new record into employee table and store the call result intoo "response" object
+            TempData["SuccessMessage"] = "Deleted Sucessfully";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ WebAPiCrud/MVC/MvcEmployeeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApimvc.Models
+{
+    public class MvcEmployeeModel
+    {
+        public int EID { get; set; }
+
+
+        [Required(ErrorMessage= "This Field is required")]
+        public string EName { get; set; }
+        public string Position { get; set; }
+        public Nullable<int> Age { get; set; }
+        public Nullable<int> Salary { get; set; }
+    }
+}
